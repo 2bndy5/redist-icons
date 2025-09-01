9161a7ee7d699ac0af92f0fca9cff43927238496
2bndy5 <14963867+2bndy5@users.noreply.github.com>
2025-09-01 07:34:21 +0000

    build: bump 2 packages in deno group

--- crates/octicons/src/py_binding/part_03.rs
+++ crates/octicons/src/py_binding/part_03.rs
@@ -3,74 +3,69 @@ use crate::icons;
 use pyo3::prelude::*;
 
 pub(super) fn bind_part_3(m: &Bound<PyModule>) -> PyResult<()> {
-    m.add(
-        "TRACKED_BY_CLOSED_COMPLETED_16",
-        icons::TRACKED_BY_CLOSED_COMPLETED_16,
-    )?;
-    m.add(
-        "TRACKED_BY_CLOSED_COMPLETED_24",
-        icons::TRACKED_BY_CLOSED_COMPLETED_24,
-    )?;
-    m.add(
-        "TRACKED_BY_CLOSED_NOT_PLANNED_16",
-        icons::TRACKED_BY_CLOSED_NOT_PLANNED_16,
-    )?;
-    m.add(
-        "TRACKED_BY_CLOSED_NOT_PLANNED_24",
-        icons::TRACKED_BY_CLOSED_NOT_PLANNED_24,
-    )?;
-    m.add("TRASH_16", icons::TRASH_16)?;
-    m.add("TRASH_24", icons::TRASH_24)?;
-    m.add("TRIANGLE_DOWN_16", icons::TRIANGLE_DOWN_16)?;
-    m.add("TRIANGLE_DOWN_24", icons::TRIANGLE_DOWN_24)?;
-    m.add("TRIANGLE_LEFT_16", icons::TRIANGLE_LEFT_16)?;
-    m.add("TRIANGLE_LEFT_24", icons::TRIANGLE_LEFT_24)?;
-    m.add("TRIANGLE_RIGHT_16", icons::TRIANGLE_RIGHT_16)?;
-    m.add("TRIANGLE_RIGHT_24", icons::TRIANGLE_RIGHT_24)?;
-    m.add("TRIANGLE_UP_16", icons::TRIANGLE_UP_16)?;
-    m.add("TRIANGLE_UP_24", icons::TRIANGLE_UP_24)?;
-    m.add("TROPHY_16", icons::TROPHY_16)?;
-    m.add("TROPHY_24", icons::TROPHY_24)?;
-    m.add("TYPOGRAPHY_16", icons::TYPOGRAPHY_16)?;
-    m.add("TYPOGRAPHY_24", icons::TYPOGRAPHY_24)?;
-    m.add("UNDO_16", icons::UNDO_16)?;
-    m.add("UNDO_24", icons::UNDO_24)?;
-    m.add("UNFOLD_16", icons::UNFOLD_16)?;
-    m.add("UNFOLD_24", icons::UNFOLD_24)?;
-    m.add("UNLINK_16", icons::UNLINK_16)?;
-    m.add("UNLINK_24", icons::UNLINK_24)?;
-    m.add("UNLOCK_16", icons::UNLOCK_16)?;
-    m.add("UNLOCK_24", icons::UNLOCK_24)?;
-    m.add("UNMUTE_16", icons::UNMUTE_16)?;
-    m.add("UNMUTE_24", icons::UNMUTE_24)?;
-    m.add("UNREAD_16", icons::UNREAD_16)?;
-    m.add("UNREAD_24", icons::UNREAD_24)?;
-    m.add("UNVERIFIED_16", icons::UNVERIFIED_16)?;
-    m.add("UNVERIFIED_24", icons::UNVERIFIED_24)?;
-    m.add("UPLOAD_16", icons::UPLOAD_16)?;
-    m.add("UPLOAD_24", icons::UPLOAD_24)?;
-    m.add("VERIFIED_16", icons::VERIFIED_16)?;
-    m.add("VERIFIED_24", icons::VERIFIED_24)?;
-    m.add("VERSIONS_16", icons::VERSIONS_16)?;
-    m.add("VERSIONS_24", icons::VERSIONS_24)?;
-    m.add("VIDEO_16", icons::VIDEO_16)?;
-    m.add("VIDEO_24", icons::VIDEO_24)?;
-    m.add("WEBHOOK_16", icons::WEBHOOK_16)?;
-    m.add("WORKFLOW_16", icons::WORKFLOW_16)?;
-    m.add("WORKFLOW_24", icons::WORKFLOW_24)?;
-    m.add("X_12", icons::X_12)?;
-    m.add("X_16", icons::X_16)?;
-    m.add("X_24", icons::X_24)?;
-    m.add("X_CIRCLE_16", icons::X_CIRCLE_16)?;
-    m.add("X_CIRCLE_24", icons::X_CIRCLE_24)?;
-    m.add("X_CIRCLE_FILL_12", icons::X_CIRCLE_FILL_12)?;
-    m.add("X_CIRCLE_FILL_16", icons::X_CIRCLE_FILL_16)?;
+    m.add("SIDEBAR_EXPAND_24", icons::SIDEBAR_EXPAND_24)?;
+    m.add("PULSE_24", icons::PULSE_24)?;
+    m.add("ISSUE_DRAFT_24", icons::ISSUE_DRAFT_24)?;
+    m.add("DUPLICATE_24", icons::DUPLICATE_24)?;
+    m.add("LOCK_24", icons::LOCK_24)?;
+    m.add("FOLD_DOWN_16", icons::FOLD_DOWN_16)?;
+    m.add("FEED_STAR_16", icons::FEED_STAR_16)?;
+    m.add("DUPLICATE_16", icons::DUPLICATE_16)?;
+    m.add("DEVICE_DESKTOP_24", icons::DEVICE_DESKTOP_24)?;
+    m.add("PIN_16", icons::PIN_16)?;
+    m.add("DEVICE_MOBILE_16", icons::DEVICE_MOBILE_16)?;
+    m.add("FILTER_REMOVE_16", icons::FILTER_REMOVE_16)?;
+    m.add("LINK_24", icons::LINK_24)?;
+    m.add("LOG_16", icons::LOG_16)?;
+    m.add("SPONSOR_TIERS_16", icons::SPONSOR_TIERS_16)?;
+    m.add("CHEVRON_UP_12", icons::CHEVRON_UP_12)?;
+    m.add("SQUARE_CIRCLE_16", icons::SQUARE_CIRCLE_16)?;
+    m.add("HUBOT_16", icons::HUBOT_16)?;
+    m.add("CALENDAR_16", icons::CALENDAR_16)?;
     m.add("X_CIRCLE_FILL_24", icons::X_CIRCLE_FILL_24)?;
-    m.add("ZAP_16", icons::ZAP_16)?;
-    m.add("ZAP_24", icons::ZAP_24)?;
-    m.add("ZOOM_IN_16", icons::ZOOM_IN_16)?;
-    m.add("ZOOM_IN_24", icons::ZOOM_IN_24)?;
+    m.add("IMAGE_24", icons::IMAGE_24)?;
+    m.add("LIST_ORDERED_16", icons::LIST_ORDERED_16)?;
+    m.add("MOVE_TO_END_24", icons::MOVE_TO_END_24)?;
+    m.add("COLUMNS_16", icons::COLUMNS_16)?;
+    m.add("SPACE_24", icons::SPACE_24)?;
+    m.add("SHARE_24", icons::SHARE_24)?;
+    m.add("SYNC_16", icons::SYNC_16)?;
+    m.add("GOAL_16", icons::GOAL_16)?;
+    m.add("QUESTION_24", icons::QUESTION_24)?;
+    m.add("SQUARE_CIRCLE_24", icons::SQUARE_CIRCLE_24)?;
+    m.add("FILE_CODE_16", icons::FILE_CODE_16)?;
     m.add("ZOOM_OUT_16", icons::ZOOM_OUT_16)?;
-    m.add("ZOOM_OUT_24", icons::ZOOM_OUT_24)?;
+    m.add("THUMBSDOWN_24", icons::THUMBSDOWN_24)?;
+    m.add("SPONSOR_TIERS_24", icons::SPONSOR_TIERS_24)?;
+    m.add("ACCESSIBILITY_24", icons::ACCESSIBILITY_24)?;
+    m.add("STOPWATCH_16", icons::STOPWATCH_16)?;
+    m.add("ARROW_BOTH_16", icons::ARROW_BOTH_16)?;
+    m.add("HEART_FILL_16", icons::HEART_FILL_16)?;
+    m.add("PERSON_ADD_24", icons::PERSON_ADD_24)?;
+    m.add("SCREEN_FULL_24", icons::SCREEN_FULL_24)?;
+    m.add("NORTH_STAR_16", icons::NORTH_STAR_16)?;
+    m.add("CPU_16", icons::CPU_16)?;
+    m.add("FILE_DIRECTORY_24", icons::FILE_DIRECTORY_24)?;
+    m.add("BUG_24", icons::BUG_24)?;
+    m.add("FEED_MERGED_16", icons::FEED_MERGED_16)?;
+    m.add("DASH_24", icons::DASH_24)?;
+    m.add("SHIELD_LOCK_16", icons::SHIELD_LOCK_16)?;
+    m.add("NO_ENTRY_24", icons::NO_ENTRY_24)?;
+    m.add("RSS_16", icons::RSS_16)?;
+    m.add("DEVICE_CAMERA_VIDEO_24", icons::DEVICE_CAMERA_VIDEO_24)?;
+    m.add("TELESCOPE_FILL_16", icons::TELESCOPE_FILL_16)?;
+    m.add("FEED_FORKED_16", icons::FEED_FORKED_16)?;
+    m.add("STACK_24", icons::STACK_24)?;
+    m.add("PROJECT_ROADMAP_24", icons::PROJECT_ROADMAP_24)?;
+    m.add("BOOKMARK_SLASH_FILL_16", icons::BOOKMARK_SLASH_FILL_16)?;
+    m.add("BRIEFCASE_16", icons::BRIEFCASE_16)?;
+    m.add(
+        "GIT_PULL_REQUEST_DRAFT_16",
+        icons::GIT_PULL_REQUEST_DRAFT_16,
+    )?;
+    m.add("UNREAD_24", icons::UNREAD_24)?;
+    m.add("LINK_16", icons::LINK_16)?;
+    m.add("CODE_REVIEW_16", icons::CODE_REVIEW_16)?;
+    m.add("SPARKLE_FILL_16", icons::SPARKLE_FILL_16)?;
     Ok(())
 }
