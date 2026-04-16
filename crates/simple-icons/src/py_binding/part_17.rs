63968f2df44e0b79bbddce54d671cde0a0ce1f80
Brendan <2bndy5@gmail.com>
2026-04-16 01:42:15 -0700

    build: bump 2 packages in deno group (#104)
    Co-authored-by: 2bndy5 <14963867+2bndy5@users.noreply.github.com>

--- crates/simple-icons/src/py_binding/part_17.rs
+++ crates/simple-icons/src/py_binding/part_17.rs
@@ -3,25 +3,28 @@ use crate::icons;
 use pyo3::prelude::*;
 
 pub(super) fn bind_part_17(m: &Bound<PyModule>) -> PyResult<()> {
-    m.add("SI_SKETCH", icons::SI_SKETCH)?;
-    m.add("SI_NFCORE", icons::SI_NFCORE)?;
-    m.add("SI_BITWIG", icons::SI_BITWIG)?;
-    m.add("SI_STMICROELECTRONICS", icons::SI_STMICROELECTRONICS)?;
-    m.add("SI_CORELDRAW", icons::SI_CORELDRAW)?;
-    m.add("SI_STARLINGBANK", icons::SI_STARLINGBANK)?;
-    m.add("SI_LIBREOFFICEIMPRESS", icons::SI_LIBREOFFICEIMPRESS)?;
-    m.add("SI_CODER", icons::SI_CODER)?;
-    m.add("SI_APARAT", icons::SI_APARAT)?;
-    m.add("SI_GOOGLECAST", icons::SI_GOOGLECAST)?;
-    m.add("SI_OMARCHY", icons::SI_OMARCHY)?;
-    m.add("SI_LIONAIR", icons::SI_LIONAIR)?;
-    m.add("SI_SEQUELIZE", icons::SI_SEQUELIZE)?;
-    m.add("SI_OCAML", icons::SI_OCAML)?;
-    m.add("SI_LARAGON", icons::SI_LARAGON)?;
-    m.add("SI_GCORE", icons::SI_GCORE)?;
-    m.add("SI_TINKERCAD", icons::SI_TINKERCAD)?;
-    m.add("SI_CREATIVECOMMONS", icons::SI_CREATIVECOMMONS)?;
-    m.add("SI_WEBMIN", icons::SI_WEBMIN)?;
-    m.add("SI_ROTTENTOMATOES", icons::SI_ROTTENTOMATOES)?;
+    m.add("SI_MOQUPS", icons::SI_MOQUPS)?;
+    m.add("SI_IPFS", icons::SI_IPFS)?;
+    m.add("SI_MASERATI", icons::SI_MASERATI)?;
+    m.add("SI_PLAYCANVAS", icons::SI_PLAYCANVAS)?;
+    m.add("SI_NOKIA", icons::SI_NOKIA)?;
+    m.add("SI_YOUTUBESTUDIO", icons::SI_YOUTUBESTUDIO)?;
+    m.add("SI_TOPTAL", icons::SI_TOPTAL)?;
+    m.add("SI_MOBX", icons::SI_MOBX)?;
+    m.add("SI_RAPID", icons::SI_RAPID)?;
+    m.add("SI_RSTUDIOIDE", icons::SI_RSTUDIOIDE)?;
+    m.add("SI_CODACY", icons::SI_CODACY)?;
+    m.add("SI_NEBULA", icons::SI_NEBULA)?;
+    m.add("SI_CLINE", icons::SI_CLINE)?;
+    m.add("SI_SESSIONIZE", icons::SI_SESSIONIZE)?;
+    m.add("SI_ARDOUR", icons::SI_ARDOUR)?;
+    m.add("SI_WEBPACK", icons::SI_WEBPACK)?;
+    m.add("SI_DOCUSAURUS", icons::SI_DOCUSAURUS)?;
+    m.add("SI_APPIMAGE", icons::SI_APPIMAGE)?;
+    m.add("SI_SHOWPAD", icons::SI_SHOWPAD)?;
+    m.add("SI_FRIGATE", icons::SI_FRIGATE)?;
+    m.add("SI_GAMEMAKER", icons::SI_GAMEMAKER)?;
+    m.add("SI_APACHEMAVEN", icons::SI_APACHEMAVEN)?;
+    m.add("SI_EDOTLECLERC", icons::SI_EDOTLECLERC)?;
     Ok(())
 }
